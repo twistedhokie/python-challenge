a83511fa88e66e8115e7e932052148e9a8c1b6ed
twistedhokie <33531329+twistedhokie@users.noreply.github.com>
2018-03-07 18:12:50 -0500

    HW Update

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,103 +3,92 @@
 
 # In[1]:
 
-
+# import systems
 import os
 import pandas as pd
 
 
 # In[184]:
 
-
+# Reas CSV
 bankfile = pd.read_csv("budget_data_1.csv")
-bankfile2 = pd.read_csv("budget_data_2.csv")
-bankfile
+
 
 
 # In[29]:
 
-
+# find total number of months in dataset
 totalmonths = bankfile["Date"].nunique()
 totalmonths
 
 
 # In[198]:
 
-
+# find total revenue amount
 totalrev = bankfile["Revenue"].sum()
 totalrev
 
 
 # In[186]:
 
-
-bankfile_copy = bankfile
-bankfile
-
-
+# find revenue change month over month
 for i in range(1,len(bankfile)):
     print((bankfile.loc[i,"Revenue"] -bankfile.loc[i-1,"Revenue"]))
     bankfile.loc[i,"Change"] = bankfile.loc[i,"Revenue"] -bankfile.loc[i-1,"Revenue"]
 
 
-# In[188]:
-
-
-bankfile
-
-
 # In[316]:
 
-
+# find lication of largest revenue change
 Max_Indx = bankfile["Change"].idxmax()
 Max_Indx
 
 
 # In[303]:
 
-
+# find value of largest revenue change
 Max_Change = bankfile["Change"].max()
 Max_Change
 
 
 # In[317]:
 
-
+# find corresponding date for largest revenue change
 Max_Date = bankfile["Date"][Max_Indx]
 Max_Date
 
 
 # In[309]:
 
-
+# find location of smallest revenue change
 Min_Indx = bankfile["Change"].idxmin()
 Min_Indx
 
 
 # In[310]:
 
-
+# find value of smallest revenue change
 Min_Change = bankfile["Change"].min()
 Min_Change
 
 
 # In[318]:
 
-
+# find corresponding date for smallest revenue change
 Min_Date = bankfile["Date"][Min_Indx]
 Min_Date
 
 
 # In[269]:
 
-
+# find average revenue change
 AvgChange = bankfile["Change"].mean()
 AvgChange
 
 
 # In[319]:
 
-
+# print summary
 print("Financial Analysis")
 print("------------------")
 print("Total Months: " + str(totalmonths))
